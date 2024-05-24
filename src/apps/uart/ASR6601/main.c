08ef09c7de76459f55a3519db6ebb8e4131c217d
Alexander Lubitskiy <lalekz@yandex.ru>
2024-05-24 18:49:00 +0200

    uart app stub

---
+++ src/apps/uart/ASR6601/main.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include "board.h"
+#include "delay.h"
+
+/**
+ * Main application entry point.
+ */
+int main( void )
+{
+    uint8_t i;
+
+    // Target board initialization
+    BoardInitMcu( );
+    BoardInitPeriph( );
+
+    while( 1 )
+    {
+        printf("Hello, world!\n");
+        DelayMs( 1000 );
+    }
+}
