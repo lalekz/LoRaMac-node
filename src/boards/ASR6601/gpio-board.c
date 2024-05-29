2cbd6644756e5586e9dbf178e6f2967cdf0397c1
MarkRanton1917 <10letdom@gmail.com>
2024-05-29 09:59:30 +0300

    writed gpio module

--- src/boards/ASR6601/gpio-board.c
+++ src/boards/ASR6601/gpio-board.c
@@ -1,4 +1,50 @@
 #include "gpio-board.h"
-void GpioMcuToggle(Gpio_t *obj) {
+#include "tremo_gpio.h"
+#include "tremo_rcc.h"
+//ACHTUNG
+//NO SUPPORT FOR AF
+//BSR IS ACTUALLY BSRR
+
+gpio_t* get_gpio_address(Gpio_t *obj) {return GPIO_BASE + (obj->portIndex) * 0x400;}
+
+void GpioMcuInit(Gpio_t *obj, PinNames pin, PinModes mode, PinConfigs config, PinTypes type, uint32_t value) {
+  gpio_t* gpiox = get_gpio_address(obj);
+  uint16_t gpio_pin = 1 << (obj->pinIndex);
+
   
+  switch(mode){
+    case PIN_INPUT:
+      gpiox->IER |= gpio_pin;
+    case PIN_OUTPUT:
+      gpiox->OER |= gpio_pin;
+
+    case PIN_ALTERNATE_FCT:
+      break;
+    case PIN_ANALOGIC:
+        gpiox->OER |= gpio_pin;
+        gpiox->IER &= ~gpio_pin;
+        gpiox->PER &= ~gpio_pin;
+      break;
+  }
+  switch(config) {
+    case PIN_PUSH_PULL:
+      break;
+    case PIN_OPEN_DRAIN:
+      gpiox->OTYPER |= gpio_pin;
+  }
+  switch(type) {
+    case PIN_NO_PULL:
+      break;
+    case PIN_PULL_UP:
+      gpiox->PSR |= gpio_pin;
+    case PIN_PULL_DOWN:
+      gpiox->PER |= gpio_pin;
+  }
+}
+
+void GpioMcuToggle(Gpio_t *obj) {
+  gpio_t* gpiox = get_gpio_address(obj);
+  uint16_t gpio_pin = 1 << (obj->pinIndex);
+  gpiox->BSR = 0;
+  gpiox->BSR |= gpio_pin << ((gpiox->ODR) & gpio_pin ? 16 : 0);
 }
--- src/boards/ASR6601/lpm-board.c
+++ src/boards/ASR6601/lpm-board.c
@@ -1,4 +1,113 @@
 #include "lpm-board.h" 
-void LpmEnterLowPower() {
+#include "board.h"
 
+static uint32_t StopModeDisable = 0;
+static uint32_t OffModeDisable = 0;
+
+void LpmSetOffMode( LpmId_t id, LpmSetMode_t mode )
+{
+    CRITICAL_SECTION_BEGIN( );
+
+    switch(mode)
+    {
+        case LPM_DISABLE:
+        {
+            OffModeDisable |= ( uint32_t )id;
+            break;
+        }
+        case LPM_ENABLE:
+        {
+            OffModeDisable &= ~( uint32_t )id;
+            break;
+        }
+        default:
+        {
+            break;
+        }
+    }
+
+    CRITICAL_SECTION_END( );
+    return;
+}
+
+void LpmSetStopMode( LpmId_t id, LpmSetMode_t mode )
+{
+    CRITICAL_SECTION_BEGIN( );
+
+    switch( mode )
+    {
+        case LPM_DISABLE:
+        {
+            StopModeDisable |= ( uint32_t )id;
+            break;
+        }
+        case LPM_ENABLE:
+        {
+            StopModeDisable &= ~( uint32_t )id;
+            break;
+        }
+        default:
+        {
+            break;
+        }
+    }
+
+    CRITICAL_SECTION_END( );
+    return;
+}
+
+void LpmEnterLowPower( void )
+{
+    if( StopModeDisable != 0 )
+    {
+        /*!
+        * SLEEP mode is required
+        */
+        LpmEnterSleepMode( );
+        LpmExitSleepMode( );
+    }
+    else
+    { 
+        if( OffModeDisable != 0 )
+        {
+            /*!
+            * STOP mode is required
+            */
+            LpmEnterStopMode( );
+            LpmExitStopMode( );
+        }
+        else
+        {
+            /*!
+            * OFF mode is required
+            */
+            LpmEnterOffMode( );
+            LpmExitOffMode( );
+        }
+    }
+    return;
+}
+
+void LpmEnterSleepMode()
+{
+}
+
+void LpmExitSleepMode()
+{
+}
+
+void LpmEnterStopMode()
+{
+}
+
+void LpmExitStopMode()
+{
+}
+
+void LpmEnterOffMode()
+{
+}
+
+void LpmExitOffMode()
+{
 }
