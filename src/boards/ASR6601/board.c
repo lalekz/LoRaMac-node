60a9730bf63520b3b9becd72dc71618dd5424dcc
MarkRanton1917 <10letdom@gmail.com>
2024-06-06 14:42:26 +0300

    added i2c support

--- src/boards/ASR6601/board.c
+++ src/boards/ASR6601/board.c
@@ -11,13 +11,16 @@
 #include "rtc-board.h"
 #include "sx126x-board.h"
 #include "uart.h"
+#include "i2c-board.h"
 
 Gpio_t Led1; //PA4
 Gpio_t Led2; //PA5
-Uart_t Uart0; //PB0 PB1
 Gpio_t LoraRfswCtrl; //PD11
 Gpio_t LoraRfswVdd; //PA10
-Spi_t Spi1;
+
+Uart_t Uart0; //PB0 PB1
+Spi_t Spi1; //PB10 PB11 PB8 PB9
+I2c_t I2c1; //PB14 PB15
 
 void LoracInit() {
     rcc_enable_peripheral_clk(RCC_PERIPHERAL_LORA, false);
@@ -49,17 +52,25 @@ void LoracInit() {
 void BoardInitMcu() {  
   GpioInit(&Led1, PA_4, PIN_OUTPUT, PIN_PUSH_PULL, PIN_PULL_DOWN, 0);
   GpioInit(&Led1, PA_5, PIN_OUTPUT, PIN_PUSH_PULL, PIN_PULL_DOWN, 0);
+
   UartInit(&Uart0, UART_USB_CDC, PB_1, PB_0);
   UartConfig(&Uart0, RX_TX, UART_BAUDRATE_115200, UART_8_BIT, UART_1_STOP_BIT, NO_PARITY, NO_FLOW_CTRL);
+
   rcc_enable_peripheral_clk(RCC_PERIPHERAL_PWR, true);
   rcc_enable_peripheral_clk(RCC_PERIPHERAL_SAC, true);
   LoracInit();
+
   RtcInit();
+
   SpiInit(&Spi1, SPI_1, PB_10, PB_11, PB_8, PB_9);
   SpiFormat(&Spi1, 8, 0, 0, 0);
   SpiFrequency(&Spi1, 100000);
+
   delay_init();
+
   LpmSetOffMode(LPM_UART_TX_ID, DISABLE);
+  I2cMcuInit(&I2c1, I2C_1, PB_14, PB_15);
+  I2cMcuFormat(&I2c1, MODE_I2C, I2C_DUTY_CYCLE_2, true, I2C_ACK_ADD_7_BIT, 100000);
 }
 
 void BoardInitPeriph() {
---
+++ src/boards/ASR6601/i2c-board.c
@@ -0,0 +1,136 @@
+#include "board-config.h"
+#include "i2c-board.h"
+#include "tremo_i2c.h"
+#include "board.h"
+#include "utilities.h"
+
+static I2cAddrSize I2cInternalAddrSize = I2C_ADDR_SIZE_8;
+
+i2c_t* get_i2c_address(I2cId_t i2cId) {
+  switch(i2cId){
+    case I2C_1:
+      return I2C1;
+    case I2C_2:
+      return I2C2;
+  }
+  return 0;
+}
+
+void i2c_wait_receive(i2c_t* i2c) {
+  while (!i2c_get_flag_status(i2c, I2C_FLAG_RECV_FULL));
+  i2c_clear_flag_status(i2c, I2C_FLAG_RECV_FULL);
+}
+
+void i2c_wait_send(i2c_t* i2c) {
+  while (!i2c_get_flag_status(i2c, I2C_FLAG_TRANS_EMPTY));
+  i2c_clear_flag_status(i2c, I2C_FLAG_TRANS_EMPTY);
+}
+
+void I2cMcuInit(I2c_t *obj, I2cId_t i2cId, PinNames scl, PinNames sda) {
+  obj->I2cId = i2cId;
+  GpioInit(&obj->Scl, scl, PIN_ALTERNATE_FCT, PIN_PUSH_PULL, PIN_PULL_UP, 3);
+  GpioInit(&obj->Sda, sda, PIN_ALTERNATE_FCT, PIN_PUSH_PULL, PIN_PULL_UP, 3);
+}
+
+void I2cMcuFormat( I2c_t *obj, I2cMode mode, I2cDutyCycle dutyCycle, bool I2cAckEnable, I2cAckAddrMode AckAddrMode, uint32_t I2cFrequency ) {
+  i2c_config_t config;
+  i2c_t* i2c = get_i2c_address(obj->I2cId);
+  if(!(RCC->CGR0 & (RCC_CGR0_I2C1_CLK_EN_MASK >> (uint8_t)(obj->I2cId)))) {
+    RCC->CGR0 |= RCC_CGR0_I2C1_CLK_EN_MASK >> (uint8_t)(obj->I2cId);
+  }
+  i2c_config_init(&config);
+  config.settings.master.speed = I2cFrequency > 100000 ? I2C_SPEED_FAST : I2C_SPEED_STANDARD;
+  i2c_init(i2c, &config);
+
+  if(!((i2c->CR) & I2C_CR_TWSI_UNIT_EN_MASK & I2C_CR_SCL_EN_MASK)) {
+    
+    i2c->CR |= I2C_CR_TWSI_UNIT_EN_MASK | I2C_CR_SCL_EN_MASK;
+  }
+}
+
+void I2cMcuResetBus( I2c_t *obj ) {
+  i2c_t* i2c = get_i2c_address(obj->I2cId);
+  i2c->CR = I2C_CR_BUS_RESET_REQUEST_MASK;
+  i2c->SR = 0;
+  while(i2c->SR & I2C_SR_UNIT_BUSY_MASK);
+}
+
+void I2cMcuDeInit( I2c_t *obj ) {
+  i2c_deinit(get_i2c_address(obj->I2cId));
+  GpioInit(&obj->Scl, (obj->Scl).pin, PIN_ANALOGIC, PIN_PUSH_PULL, PIN_NO_PULL, 0);
+  GpioInit(&obj->Sda, (obj->Sda).pin, PIN_ANALOGIC, PIN_PUSH_PULL, PIN_NO_PULL, 0);
+}
+
+void I2cSetAddrSize( I2c_t *obj, I2cAddrSize addrSize ) {I2cInternalAddrSize = addrSize;}
+
+LmnStatus_t I2cMcuWriteBuffer(I2c_t *obj, uint8_t deviceAddr, uint8_t *buffer, uint16_t size) {
+  uint16_t i;
+  i2c_t* i2c = get_i2c_address(obj->I2cId);
+  i2c_master_send_start(i2c, deviceAddr, I2C_WRITE);
+  i2c_wait_send(i2c);
+  
+  for(i = 0; i < size; i++) {
+    i2c_send_data(i2c, buffer[i]);
+    i2c_wait_send(i2c);
+  }
+  i2c_master_send_stop(i2c); 
+  return LMN_STATUS_OK;
+}
+
+LmnStatus_t I2cMcuReadBuffer(I2c_t *obj, uint8_t deviceAddr, uint8_t *buffer, uint16_t size) {
+  uint16_t i;
+  i2c_t* i2c = get_i2c_address(obj->I2cId);
+  i2c_master_send_start(i2c, deviceAddr, I2C_READ);
+  i2c_wait_send(i2c);
+  for(i = 0; i < size - 1; i++) {
+    i2c_set_receive_mode(i2c, I2C_ACK);
+    i2c_wait_receive(i2c);
+    buffer[i] = i2c_receive_data(i2c);
+  }
+  i2c_set_receive_mode(i2c, I2C_NAK);
+  i2c_wait_receive(i2c);
+  buffer[size - 1] = i2c_receive_data(i2c);
+  i2c_master_send_stop(i2c); 
+  return LMN_STATUS_OK;
+}
+
+LmnStatus_t I2cMcuWriteMemBuffer(I2c_t *obj, uint8_t deviceAddr, uint16_t addr, uint8_t *buffer, uint16_t size) {
+  uint16_t i;
+  i2c_t* i2c = get_i2c_address(obj->I2cId);
+  i2c_master_send_start(i2c, deviceAddr, I2C_WRITE);
+  i2c_wait_send(i2c);
+  i2c_send_data(i2c, addr);
+  i2c_wait_send(i2c);
+  for(i = 0; i < size; i++) {
+    i2c_send_data(i2c, buffer[i]);
+    i2c_wait_send(i2c);
+  }
+  i2c_master_send_stop(i2c); 
+  return LMN_STATUS_OK;
+}
+
+LmnStatus_t I2cMcuReadMemBuffer( I2c_t *obj, uint8_t deviceAddr, uint16_t addr, uint8_t *buffer, uint16_t size ) {
+  uint16_t i;
+  i2c_t* i2c = get_i2c_address(obj->I2cId);
+  i2c_master_send_start(i2c, deviceAddr, I2C_WRITE);
+  i2c_wait_send(i2c);
+  i2c_send_data(i2c, addr);
+  i2c_wait_send(i2c);
+  i2c_master_send_start(i2c, deviceAddr, I2C_READ);
+  i2c_wait_send(i2c);
+  for(i = 0; i < size - 1; i++) {
+    i2c_set_receive_mode(i2c, I2C_ACK);
+    i2c_wait_receive(i2c);
+    buffer[i] = i2c_receive_data(i2c);
+  }
+  i2c_set_receive_mode(i2c, I2C_NAK);
+  i2c_wait_receive(i2c);
+  buffer[size - 1] = i2c_receive_data(i2c);
+  i2c_master_send_stop(i2c); 
+  return LMN_STATUS_OK;
+}
+
+LmnStatus_t I2cMcuWaitStandbyState(I2c_t *obj, uint8_t deviceAddr) {
+  while(get_i2c_address(obj->I2cId)->SR & I2C_SR_UNIT_BUSY_MASK);
+  return LMN_STATUS_OK;
+}
